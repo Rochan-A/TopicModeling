e9c5e13151dae73dfae33ad492a4badd949837e6
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-05-28 15:58:22 +0530

    Uses argparse

--- model.py
+++ model.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """
-	This script is to be used to train LDA and LSA models
+	Used to train LDA and LSA models
 """
 
 #######################################
-
+from argparse import ArgumentParser
 from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
-import gensim, os, sys, codecs
+import gensim, os, codecs
 
 __author__ = "Rochan Avlur Venkat"
 #__copyright__ = ""
@@ -23,56 +23,74 @@ __email__ = "rochan170543@mechyd.ac.in"
 
 #######################################
 
-tokenizer = RegexpTokenizer(r'\w+')
+"""
+	Loads all the reviews into a list
+"""
+def loadReviews(inp_path):
+	# Get the list of files
+	os.chdir(inp_path)
+	currFileList = [f for f in os.listdir('.') if os.path.isfile(f)]
 
-# create English stop words list
-en_stop = get_stop_words('en')
-
-# Create p_stemmer of class PorterStemmer
-p_stemmer = PorterStemmer()
-
-# Get the list of files
-os.chdir(sys.argv[1])
-currFileList = [f for f in os.listdir('.') if os.path.isfile(f)]
-
-# Load all the lines
-doc_set = []
-for i in range(len(currFileList)):
+	# Load the lines
+	doc = []
+	for i in range(len(currFileList)):
 	with codecs.open(currFileList[i], "r", encoding='utf-8') as f:
 		for line in f:
-			doc_set.append(line)
+			doc.append(line)
+	return doc
 
-# list for tokenized documents in loop
-texts = []
+if __name__ == '__main__':
 
-# loop through document list
-for i in doc_set:
+	# Parse command-line arguments
+	parser = ArgumentParser()
+	parser.add_argument("-i", "--input-path",
+			help="Path to parsed reviews", type=str)
+	parser.add_argument("-o", "--output-path",
+			help="Destination of to save model", type=str)
+	args = parser.parse_args()
 
-	# clean and tokenize document string
-	raw = i.lower()
-	tokens = tokenizer.tokenize(raw)
+	# Load reviews into a list
+	doc_set = loadReviews(args.input_path)
 
-	# remove stop words from tokens
-	stopped_tokens = [i for i in tokens if not i in en_stop]
+	tokenizer = RegexpTokenizer(r'\w+')
 
-	# stem tokens
-	stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+	# create English stop words list
+	en_stop = get_stop_words('en')
 
-	# add tokens to list
-	texts.append(stemmed_tokens)
+	# Create p_stemmer of class PorterStemmer
+	p_stemmer = PorterStemmer()
 
-# turn our tokenized documents into a id <-> term dictionary
-dictionary = corpora.Dictionary(texts)
+	# list for tokenized documents in loop
+	texts = []
 
-# convert tokenized documents into a document-term matrix
-corpus = [dictionary.doc2bow(text) for text in texts]
+	# loop through document list
+	for i in doc_set:
 
-# generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=100, id2word = dictionary, passes=20)
+		# clean and tokenize document string
+		raw = i.lower()
+		tokens = tokenizer.tokenize(raw)
 
-# generate LSA model
-lsamodel = gensim.models.lsimodel.LsiModel(corpus, num_topics=100, id2word = dictionary)
+		# remove stop words from tokens
+		stopped_tokens = [i for i in tokens if not i in en_stop]
 
-# Save the models
-ldamodel.save(sys.argv[2] + "LDA")
-lsamodel.save(sys.argv[2] + "LSA")
+		# stem tokens
+		stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+		# add tokens to list
+		texts.append(stemmed_tokens)
+
+	# turn our tokenized documents into a id <-> term dictionary
+	dictionary = corpora.Dictionary(texts)
+
+	# convert tokenized documents into a document-term matrix
+	corpus = [dictionary.doc2bow(text) for text in texts]
+
+	# generate LDA model
+	ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=100, id2word = dictionary, passes=20)
+
+	# generate LSA model
+	lsamodel = gensim.models.lsimodel.LsiModel(corpus, num_topics=100, id2word = dictionary)
+
+	# Save the models
+	ldamodel.save(args.output_path + "LDA")
+	lsamodel.save(args.output_path + "LSA")
