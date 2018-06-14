11bfca4108fdb94daf4b614dbe2903a5c6b46dc4
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-06-14 14:57:12 +0530

    Scripts to interface with lda2vec

---
+++ lda2vec/lda2vec_run.py
@@ -0,0 +1,97 @@
+# Author: Chris Moody <chrisemoody@gmail.com>
+# License: MIT
+
+# This simple example loads the newsgroups data from sklearn
+# and train an LDA-like model on it
+import os.path
+import pickle
+import time
+
+import chainer
+from chainer import cuda
+from chainer import serializers
+import chainer.optimizers as O
+import numpy as np
+
+from lda2vec import utils
+from lda2vec import prepare_topics, print_top_words_per_topic
+from lda2vec_model import LDA2Vec
+
+gpu_id = int(os.getenv('CUDA_GPU', 0))
+cuda.get_device(gpu_id).use()
+print "Using GPU " + str(gpu_id)
+
+# You must run preprocess.py before this data becomes available
+vocab = pickle.load(open('vocab', 'r'))
+corpus = pickle.load(open('corpus', 'r'))
+data = np.load(open('data.npz', 'r'))
+flattened = data['flattened']
+story_id = data['story_id']
+
+# Model Parameters
+# Number of documents
+n_stories = story_id.max() + 1
+# Number of unique words in the vocabulary
+n_vocab = flattened.max() + 1
+# Number of dimensions in a single word vector
+n_units = 256
+# Number of topics to fit
+n_story_topics = 40
+batchsize = 4096
+# Get the string representation for every compact key
+words = corpus.word_list(vocab)[:n_vocab]
+
+# How many tokens are in each story
+sty_idx, lengths = np.unique(story_id, return_counts=True)
+sty_len = np.zeros(sty_idx.max() + 1, dtype='int32')
+sty_len[sty_idx] = lengths
+
+# Count all token frequencies
+tok_idx, freq = np.unique(flattened, return_counts=True)
+term_frequency = np.zeros(n_vocab, dtype='int32')
+term_frequency[tok_idx] = freq
+
+model = LDA2Vec(n_stories=n_stories, n_story_topics=n_story_topics,
+                n_units=n_units, n_vocab=n_vocab, counts=term_frequency,
+                n_samples=15)
+if os.path.exists('lda2vec.hdf5'):
+    print "Reloading from saved"
+    serializers.load_hdf5("lda2vec.hdf5", model)
+model.to_gpu()
+optimizer = O.Adam()
+optimizer.setup(model)
+clip = chainer.optimizer.GradientClipping(5.0)
+optimizer.add_hook(clip)
+
+j = 0
+epoch = 0
+fraction = batchsize * 1.0 / flattened.shape[0]
+for epoch in range(5000):
+    ts = prepare_topics(cuda.to_cpu(model.mixture_sty.weights.W.data).copy(),
+                        cuda.to_cpu(model.mixture_sty.factors.W.data).copy(),
+                        cuda.to_cpu(model.sampler.W.data).copy(),
+                        words)
+    print_top_words_per_topic(ts)
+    ts['doc_lengths'] = sty_len
+    ts['term_frequency'] = term_frequency
+    np.savez('topics.story.pyldavis', **ts)
+    for s, f in utils.chunks(batchsize, story_id, flattened):
+        t0 = time.time()
+        optimizer.zero_grads()
+        l = model.fit_partial(s.copy(), f.copy())
+        prior = model.prior()
+        loss = prior * fraction
+        loss.backward()
+        optimizer.update()
+        msg = ("J:{j:05d} E:{epoch:05d} L:{loss:1.3e} "
+               "P:{prior:1.3e} R:{rate:1.3e}")
+        prior.to_cpu()
+        loss.to_cpu()
+        t1 = time.time()
+        dt = t1 - t0
+        rate = batchsize / dt
+        logs = dict(loss=float(l), epoch=epoch, j=j,
+                    prior=float(prior.data), rate=rate)
+        print msg.format(**logs)
+        j += 1
+    serializers.save_hdf5("lda2vec.hdf5", model)
---
+++ lda2vec/preprocess.py
@@ -0,0 +1,134 @@
+from lda2vec import preprocess, Corpus
+import numpy as np
+import logging, gensim, csv, codecs
+import cPickle as pickle
+from gensim.corpora import Dictionary
+from argparse import ArgumentParser
+
+logging.basicConfig()
+
+def force_unicode(s, encoding='utf-8', errors='ignore'):
+	"""
+	Returns a unicode object representing 's'. Treats bytestrings using the
+	'encoding' codec.
+
+	Arguments
+	---------
+	s: string to be encoded
+	encoding: encoding type, defaults to `utf-8`
+	errors: whether or not to ignore errors, defaults to `ignore`
+
+	Returns
+	---------
+	unicode string
+	"""
+
+	if s is None:
+		return ''
+
+	try:
+		if not isinstance(s, basestring,):
+			if hasattr(s, '__unicode__'):
+				s = unicode(s)
+			else:
+				try:
+					s = unicode(str(s), encoding, errors)
+				except UnicodeEncodeError:
+					if not isinstance(s, Exception):
+						raise
+					# If we get to here, the caller has passed in an Exception
+					# subclass populated with non-ASCII data without special
+					# handling to display as a string. We need to handle this
+					# without raising a further exception. We do an
+					# approximation to what the Exception's standard str()
+					# output should be.
+					s = ' '.join([force_unicode(arg, encoding, errors) for arg in s])
+		elif not isinstance(s, unicode):
+			# Note: We use .decode() here, instead of unicode(s, encoding,
+			# errors), so that if s is a SafeString, it ends up being a
+			# SafeUnicode at the end.
+			s = s.decode(encoding, errors)
+	except UnicodeDecodeError, e:
+		if not isinstance(s, Exception):
+			raise UnicodeDecodeError (s, *e.args)
+		else:
+			# If we get to here, the caller has passed in an Exception
+			# subclass populated with non-ASCII bytestring data without a
+			# working unicode method. Try to handle this without raising a
+			# further exception by individually forcing the exception args
+			# to unicode.
+			s = ' '.join([force_unicode(arg, encoding, errors) for arg in s])
+	return s
+
+if __name__ == '__main__':
+
+	# Parse command-line arguments
+	parser = ArgumentParser()
+	parser.add_argument("-i", "--input-path",
+			help="Path to tokenized sentences", type=str)
+	parser.add_argument("-o", "--output-path",
+			help="Destination for parsed and preprocessed output", type=str)
+	args = parser.parse_args()
+
+	# Limit tokens per sentence
+	max_length = 100
+
+	readToken= []
+	lineno = 0
+	# Open the tokenized file
+	with codecs.open(args.input_path, 'r', encoding='utf8') as File:
+		reader=csv.reader(File)
+		for row in reader:
+			lineno = lineno + 1
+			readToken.append(row)
+
+	dic = Dictionary(readToken)
+	vocab = dict(dic)
+
+	array = np.zeros((lineno, max_length), int)
+
+	for i in range(lineno):
+		for j in range(len(readToken[i])):
+			array[i][j] = np.int64(vocab.keys()[vocab.values().index(readToken[i][j])])
+		if len(readToken[i]) < max_length:
+			for k in range(len(readToken[i]), max_length):
+				array[i][k] = -2
+	tokens = array
+
+	"""
+	    arr : 2D array of ints
+	        Has shape (len(texts), max_length). Each value represents
+	        the word index.
+	    vocab : dict
+	        Keys are the word index, and values are the string. The pad index gets
+	        mapped to None
+	"""
+
+	# Make a ranked list of rare vs frequent words
+	corpus = Corpus()
+	corpus.update_word_count(tokens)
+	corpus.finalize()
+
+	# The tokenization uses spaCy indices, and so may have gaps
+	# between indices for words that aren't present in our dataset.
+	# This builds a new compact index
+	compact = corpus.to_compact(tokens)
+	# Remove extremely rare words
+	pruned = corpus.filter_count(compact, min_count=10)
+	# Words tend to have power law frequency, so selectively
+	# downsample the most prevalent words
+	clean = corpus.subsample_frequent(pruned)
+	print "n_words", np.unique(clean).max()
+
+	story_id = np.array(list(range(0, lineno)), int)
+
+	flattened, features_flat = corpus.compact_to_flat(pruned, story_id)
+
+	story_id_f = features_flat
+
+	data = dict(flattened=flattened, story_id=story_id_f)
+
+	pickle.dump(corpus, open('corpus', 'w'), protocol=2)
+	pickle.dump(vocab, open('vocab', 'w'), protocol=2)
+	np.savez('data', **data)
+	np.save(open('tokens', 'w'), tokens)
