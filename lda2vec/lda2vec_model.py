c2586356b261c7552ae5d9c7434805abe477a1f4
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-06-15 10:55:44 +0530

    added author_id and commented

--- lda2vec/lda2vec_model.py
+++ lda2vec/lda2vec_model.py
@@ -11,12 +11,16 @@ import numpy as np
 
 class LDA2Vec(Chain):
     def __init__(self, n_stories=100, n_story_topics=10,
+#                 n_authors=100, n_author_topics=10,
                  n_units=256, n_vocab=1000, dropout_ratio=0.5, train=True,
                  counts=None, n_samples=15, word_dropout_ratio=0.0):
         em1 = EmbedMixture(n_stories, n_story_topics, n_units,
                            dropout_ratio=dropout_ratio)
+#        em2 = EmbedMixture(n_authors, n_author_topics, n_units,
+#                           dropout_ratio=dropout_ratio)
         kwargs = {}
         kwargs['mixture_sty'] = em1
+#        kwargs['mixture_aut'] = em2
         kwargs['sampler'] = L.NegativeSampling(n_units, counts, n_samples)
         super(LDA2Vec, self).__init__(**kwargs)
         rand = np.random.random(self.sampler.W.data.shape)
@@ -29,7 +33,8 @@ class LDA2Vec(Chain):
 
     def prior(self):
         dl1 = dirichlet_likelihood(self.mixture_sty.weights)
-        return dl1
+#        dl2 = dirichlet_likelihood(self.mixture_aut.weights)
+        return dl1# + dl2
 
     def fit_partial(self, rsty_ids, raut_ids, rwrd_ids, window=5):
         sty_ids, aut_ids, wrd_ids = move(self.xp, rsty_ids, raut_ids, rwrd_ids)
@@ -37,10 +42,12 @@ class LDA2Vec(Chain):
         pivot = F.embed_id(pivot_idx, self.sampler.W)
         sty_at_pivot = rsty_ids[window: -window]
         aut_at_pivot = raut_ids[window: -window]
+        print(move(self.xp, sty_at_pivot))
         sty = self.mixture_sty(next(move(self.xp, sty_at_pivot)))
+#        aut = self.mixture_aut(next(move(self.xp, aut_at_pivot)))
         loss = 0.0
         start, end = window, rwrd_ids.shape[0] - window
-        context = sty + F.dropout(pivot, self.dropout_ratio)
+        context = sty + F.dropout(pivot, self.dropout_ratio)# + aut
         for frame in range(-window, window + 1):
             # Skip predicting the current pivot
             if frame == 0:
