2690de67f8fb45abd57c6c2f3f64dc4cd65933d3
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-06-26 15:35:07 +0530

    Compute Hellingers distance and plot topic vectors

---
+++ postprocess/distance.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	Used to graph the Hellingers distance between topic vectors
+"""
+
+#######################################
+
+import csv, gensim
+import numpy as np
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+from sklearn import manifold
+
+__author__ = "Rochan Avlur Venkat"
+#__copyright__ = ""
+#__credits__ = [""]
+#__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Rochan Avlur Venkat"
+__email__ = "rochan170543@mechyd.ac.in"
+
+#######################################
+
+if __name__ == '__main__':
+
+	# Parse command-line arguments
+	parser = ArgumentParser()
+	parser.add_argument("-i", "--input-path", \
+			help="Path to LDA Mallet", type=str)
+	parser.add_argument("-n", "--topic-number", \
+			help="number of topics", type=int, default=100)
+	parser.add_argument("-t", "--top-words", \
+			help="number of top terms/words per topic to calculate for", type=int, default=10)
+	parser.add_argument("-a", "--alpha", \
+			help="Alpha value", type=float, default=5.0)
+	parser.add_argument("-o", "--output-path", \
+			help="Destination to topic word weights", type=str)
+	ARGS = parser.parse_args()
+
+	# Load the LDA mallet model
+	lda_mallet = gensim.utils.SaveLoad.load(ARGS.input_path)
+
+	# Construct an empty matrix
+	matrix = np.zeros((len(lda_mallet.get_topics()), len(lda_mallet.get_topics()) + 1), dtype=float)
+
+	# Calculate the Hellinger Distance between all pariwise topic vectors
+	for i in range(0, len(lda_mallet.get_topics())):
+		for j in range(1, len(lda_mallet.get_topics()) + 1):
+			matrix[i][j] = (gensim.matutils.hellinger(lda_mallet.get_topics()[i], lda_mallet.get_topics()[j-1]))
+
+	# Add labels to each row
+	for i in range(len(lda_mallet.get_topics())):
+		matrix[i][0] = i
+
+	# Print the matrix
+	print(matrix)
+
+	# Save the matrix
+	with open('distance.csv', 'w') as F:
+		spamwriter = csv.writer(F, delimiter=',')
+		for row in matrix:
+			spamwriter.writerow(row)
+
+	data = matrix
+
+	# Separate the distance values from the labels
+	dists = []
+	cities = []
+	for d in data:
+		cities.append(d[0])
+		dists.append(map(float, d[1:]))
+
+	adist = np.array(dists)
+	amax = np.amax(adist)
+	adist /= amax
+
+	mds = manifold.MDS(n_components=2, dissimilarity="precomputed", random_state=6)
+	results = mds.fit(adist)
+
+	coords = results.embedding_
+
+	plt.subplots_adjust(bottom=0.1)
+	plt.scatter(coords[:, 0], coords[:, 1], marker='o')
+	for label, x, y in zip(cities, coords[:, 0], coords[:, 1]):
+		plt.annotate( \
+			label, \
+			xy=(x, y), xytext=(-20, 20), \
+			textcoords='offset points', ha='right', va='bottom', \
+			bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5), \
+			arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
+
+	plt.show()
